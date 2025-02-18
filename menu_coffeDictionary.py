fedec5715a8dd2163eb9a43cc3fb5bdd843a9023
kesyaanugrask17 <anugraskkesya@gmail.com>
2025-02-18 13:24:35 +0700

    First upload

---
+++ menu_coffeDictionary.py
@@ -0,0 +1,25 @@
+print("=========================")
+print("Hewan Peliharaan")
+print("By kesya 9B")
+print("=========================")
+
+"Hewan Peliharaan Favorit"={
+    "Anjing":  15000000,
+    "Kelinci": 100,
+    "landak mini": 9000,
+    "hamster": 800,
+    "guinea pig": 6000,
+    "otter": 54000,
+    "Kura-kura": 7000,
+    "Kucing": 5000,
+}
+
+List_hewan=input("Anjing,Kelinci,Landak mini,hamster,guinea pig,otter,Kura-kura,Kucing")
+pilih_hewan=input("pilihlah hewan favorit kamu :")
+if pilih-musik in musik favorit:
+    print(f"hewan {pilih_hewan}"kamu berharap Rp (hewan))
+    print(f"pilih hewanmu {hewan_pilihan}")
+else:
+    print("Silahkan pilih hewan faviritemu":"Anjing,Kelinci,Landak mini,hamster,guinea pig,otter,Kura-kura,Kucing")
+
+    print("Terima kasih")
